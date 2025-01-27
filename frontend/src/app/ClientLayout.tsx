eec94a5bd1b9e437977de13d85fd644bd233fa6b
solji <solji.choi89@gmail.com>
2025-01-27 12:43:18 +0900

    관리자 페이지인지 여부에 따라서 레이아웃을 분기, accessToken 만료시간 변경

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -3,6 +3,7 @@
 import { components } from '@/lib/backend/apiV1/schema'
 import client from '@/lib/backend/client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function ClientLayout({
   children,
@@ -15,6 +16,10 @@ export default function ClientLayout({
   isLogin: boolean
   isAdmin: boolean
 }>) {
+  const pathname = usePathname()
+  const isAdmPage = pathname.startsWith('/adm')
+  const isUserPage = !isAdmPage
+
   const logout = async () => {
     const response = await client.DELETE('/api/v1/members/logout')
 
@@ -30,15 +35,27 @@ export default function ClientLayout({
     <>
       <header className="border-[2px] border-[red] p-5">
         <div className="flex gap-4">
-          <Link href="/">홈</Link>
-          <Link href="/about">소개</Link>
-          <Link href="/post/list">공개글</Link>
-          {isLogin && <Link href="/post/mine">내 글</Link>}
-          {!isLogin && <Link href="/member/join">회원가입</Link>}
-          {!isLogin && <Link href="/member/login">로그인</Link>}
-          {isLogin && <button onClick={logout}>로그아웃</button>}
-          {isLogin && <Link href="/member/me">{me.nickname}님 정보</Link>}
-          {isAdmin && <Link href="/adm/member/list">관리자</Link>}
+          {isUserPage && (
+            <>
+              <Link href="/">홈</Link>
+              <Link href="/about">소개</Link>
+              <Link href="/post/list">공개글</Link>
+              {isLogin && <Link href="/post/mine">내 글</Link>}
+              {!isLogin && <Link href="/member/join">회원가입</Link>}
+              {!isLogin && <Link href="/member/login">로그인</Link>}
+              {isLogin && <button onClick={logout}>로그아웃</button>}
+              {isLogin && <Link href="/member/me">{me.nickname}님 정보</Link>}
+              {isAdmin && <Link href="/adm">관리자</Link>}
+            </>
+          )}
+          {isAdmPage && (
+            <>
+              <Link href="/adm">관리자 홈</Link>
+              <Link href="/">홈</Link>
+              <button onClick={logout}>로그아웃</button>
+              <Link href="/adm/member/list">회원 목록</Link>
+            </>
+          )}
         </div>
       </header>
 
@@ -47,7 +64,8 @@ export default function ClientLayout({
       </main>
 
       <footer className="border-[2px] border-[pink] p-5">
-        Copyright 2025.
+        {isUserPage && <div>Copyright 2025.</div>}
+        {isAdmPage && <div>Copyright 2025. 관리자</div>}
       </footer>
     </>
   )
